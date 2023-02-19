14ac2172afd151a90183584a09d5652ff8fbecf4
Vasyl Arkhipov <vbitseps02@gmail.com>
2023-02-19 22:56:01 +0200

    simple project setting in 'pom.xml', 'hibernate.cfg.xml' and first entity User at all

---
+++ src/main/java/model/User.java
@@ -0,0 +1,44 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
